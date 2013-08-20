4ca63234d0ef0f325b11afd6484dd07b98641c77
DoubleJ <doublej.k7@gmail.com>
2013-08-20 21:32:52 +0900

    add Example Acitivty

---
+++ src/com/tsinform/Activity1.java
@@ -0,0 +1,14 @@
+package com.tsinform;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity1 extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+	}
+
+}
--- src/com/tsinform/MainActivity.java
+++ src/com/tsinform/MainActivity.java
@@ -1,6 +1,8 @@
 package com.tsinform;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -10,15 +12,20 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 	
 	OnClickListener clickListener = new MainClickListener();
+	private Context mContext;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mContext = this;
 
 		findViewById(R.id.button1).setOnClickListener(clickListener);
 		findViewById(R.id.button2).setOnClickListener(clickListener);
 		findViewById(R.id.button3).setOnClickListener(clickListener);
+		findViewById(R.id.button4).setOnClickListener(clickListener);
+		findViewById(R.id.button5).setOnClickListener(clickListener);
 	}
 
 	@Override
@@ -33,20 +40,39 @@ public class MainActivity extends Activity {
 		public void onClick(View v) {
 			String msg = "";
 			switch (v.getId()) {
-			case R.id.button1:
+			case R.id.button1: {
 				// 교통정보API
 				msg = "교통정보API";
+				Toast.makeText(getApplicationContext(), String.format("%s", msg), Toast.LENGTH_SHORT).show();
+			}
 				break;
-			case R.id.button2:
+			case R.id.button2:{
 				// 구글맵API
 				msg = "구글맵API";
+				Toast.makeText(getApplicationContext(), String.format("%s", msg), Toast.LENGTH_SHORT).show();
+			}
 				break;
-			case R.id.button3:
+			case R.id.button3: {
 				// 페이스북API
-				msg = "페이스북API";;
+				msg = "페이스북API";
+				Toast.makeText(getApplicationContext(), String.format("%s", msg), Toast.LENGTH_SHORT).show();
+			}
+				break;
+			case R.id.button4: {
+				// DataBase
+				msg = "DataBase";
+				Toast.makeText(getApplicationContext(), String.format("%s", msg), Toast.LENGTH_SHORT).show();
+			}
+				break;
+			case R.id.button5: {
+				// Activity
+				Intent intent = new Intent(mContext, Activity1.class);
+//				Intent intent = new Intent(MainActivity.this, cls)
+				startActivity(intent);
+			}
 				break;
 			}
-			Toast.makeText(getApplicationContext(), String.format("%s", msg), Toast.LENGTH_SHORT).show();
+			
 		}
 	}
 }
