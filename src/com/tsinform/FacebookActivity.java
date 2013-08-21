ceb7ce3d212be76cc62f61c6638b2d7700337f07
kang hwan ki <xangfi@gmail.com>
2013-08-21 15:26:55 +0900

    Facebook 연동 진행중...

--- src/com/tsinform/FacebookActivity.java
+++ src/com/tsinform/FacebookActivity.java
@@ -2,11 +2,16 @@ package com.tsinform;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class FacebookActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_facebook);
+
+		TextView textView = (TextView)findViewById(R.id.fbText);
+		textView.setText("facebook");
 	}
 }
