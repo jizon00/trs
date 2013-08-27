33fe3bd8ed21da9c28940bfd9926bd23cf642c57
kang hwan ki <xangfi@gmail.com>
2013-08-27 20:36:50 +0900

    intro runnable 처리.

--- src/com/tsinform/IntroActivity.java
+++ src/com/tsinform/IntroActivity.java
@@ -1,12 +1,43 @@
 package com.tsinform;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class IntroActivity extends Activity {
+	private Context mContext;
+	private Handler mHnd;
+	private Runnable mRunnable;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_intro);
+		
+		mContext = this;
+		
+		mHnd = new Handler();
+		mRunnable = new IntroRunnable();
+		
+		mHnd.postDelayed(mRunnable, 1000);
+	}
+	
+	class IntroRunnable implements Runnable {
+		@Override
+		public void run() {
+			Intent intent = new Intent(mContext, MainActivity.class);
+			startActivity(intent);
+			finish();
+			
+			overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+		}
+	}
+	
+	@Override
+	public void onBackPressed() {
+		super.onBackPressed();
+		mHnd.removeCallbacks(mRunnable);
 	}
 }
